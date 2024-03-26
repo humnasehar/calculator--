b751b7b1568682b4cb450db97f7525b7673652ad
humna <160516050+humnasehar@users.noreply.github.com>
2024-03-26 09:30:01 -0700

    //simple calculator

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const userInput = await inquirer.prompt([{message: "Enter a  first number:", type: "number", name: "firstNumber"},
+{ message: "Enter a secondnumber:", type: "number" , name :"secondNumber"}, 
+
+{
+    message: "Select just one operator to perform an action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition","Subtraction", "Multiplication", "Division"]
+},   
+
+]);
+
+if (userInput.operator ==="Addition") {
+    console.log(userInput.firstNumber +userInput.secondNumber); 
+    } else if (userInput.operator ==="Subtraction") {
+        console.log(userInput.firstNumber - userInput.secondNumber);
+    }else if (userInput.operator ==="Multiplication") {
+        console.log(userInput.firstNumber * userInput.secondNumber);
+    }else if (userInput.operator === "Division") {
+       console.log(userInput.firstNumber / userInput.secondNumber);
+    } else {
+        console.log("please select a valid operator");
+    }
+
+
+
+        
+    
+
+
+
+
